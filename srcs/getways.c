72634e4ba6c61d4ca32bd63f494f111c6b840d28
bhivert <hivert.benoit@gmail.com>
2016-05-23 10:52:20 +0200

    up

--- srcs/getways.c
+++ srcs/getways.c
@@ -6,7 +6,7 @@
 /*   By: bhivert <bhivert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/15 10:23:58 by bhivert           #+#    #+#             */
-/*   Updated: 2016/05/22 14:42:46 by bhivert          ###   ########.fr       */
+/*   Updated: 2016/05/23 10:47:29 by bhivert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,32 +27,51 @@ static size_t	getpossibility(t_lemin *e, size_t room)
 	return (n);
 }
 
-static void		getways_rec(t_lemin *e, t_container *w, size_t current_room)
+t_container		*create_begins(t_lemin *e, t_container *current_way, \
+					size_t current_room)
 {
 	size_t		i;
 	size_t		possibility;
-	t_container	*begins;
 	t_container	*tmp;
+	t_container	*begins;
+
+	if (!(begins = ft_new_container(VECTOR, sizeof(t_container *))))
+		badalloc(__FILE__, __LINE__);
+	possibility = getpossibility(e, current_room);
+	while (++i < possibility)
+	{
+		if (!(tmp = ft_copy(current_way)))
+			badalloc(__FILE__, __LINE__);
+		ft_push_back(begins, &tmp);
+	}
+	return (begins);
+}
+
+static size_t	getnextpossibility(t_lemin *e, t_container *current_room, \
+					size_t possibility)
+{
+	return (-1);
+}
+
+static void		getways_rec(t_lemin *e, t_container *current_way, \
+					size_t current_room)
+{
+	size_t		i;
+	t_container	*begins;
+	t_container	**b;
 
 	i = (size_t)-1;
 	if (current_room == e->end->id)
-		ft_push_back(e->ways, &w);
+		ft_push_back(e->ways, &current_way);
 	else
 	{
-		if (!(begins = ft_new_container(VECTOR, sizeof(t_container *))))
-			badalloc(__FILE__, __LINE__);
-
-		possibility = getpossibility(e, current_room);
-		while (++i < possibility)
+		while (++i < ft_size(begins))
 		{
-			if (!(tmp = ft_copy(w)))
-				badalloc(__FILE__, __LINE__);
-			ft_push_back(begins, &tmp);
-		}
-
-		while (++i < ft_size(e->rooms))
-		{
-			;
+			begins = create_begins(e, current_way, current_room);
+			if ((b = ft_at_index(begins, 0)))
+			{
+				;
+			}
 		}
 		// push possibility
 
