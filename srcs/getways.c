5104fe548c67d25bb4ecfbccb7cc244ac73fc942
benoit HIVERT <hivert.benoit@gmail.com>
2016-05-23 03:24:28 +0200

    up

--- srcs/getways.c
+++ srcs/getways.c
@@ -6,7 +6,7 @@
 /*   By: bhivert <bhivert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/15 10:23:58 by bhivert           #+#    #+#             */
-/*   Updated: 2016/05/20 15:08:16 by bhivert          ###   ########.fr       */
+/*   Updated: 2016/05/22 14:42:46 by bhivert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,40 +27,37 @@ static size_t	getpossibility(t_lemin *e, size_t room)
 	return (n);
 }
 
-static size_t	getnextid(t_lemin *e, t_container *w, size_t room)
-{
-	t_room	**last;
-
-//	last = ft_at_index();
-}
-
 static void		getways_rec(t_lemin *e, t_container *w, size_t current_room)
 {
 	size_t		i;
 	size_t		possibility;
 	t_container	*begins;
+	t_container	*tmp;
 
 	i = (size_t)-1;
-	if (!(begins = ft_new_container(VECTOR, sizeof(t_container *))))
-		badalloc(__FILE__, __LINE__);
 	if (current_room == e->end->id)
 		ft_push_back(e->ways, &w);
 	else
 	{
+		if (!(begins = ft_new_container(VECTOR, sizeof(t_container *))))
+			badalloc(__FILE__, __LINE__);
+
 		possibility = getpossibility(e, current_room);
 		while (++i < possibility)
 		{
-
+			if (!(tmp = ft_copy(w)))
+				badalloc(__FILE__, __LINE__);
+			ft_push_back(begins, &tmp);
 		}
-		ft_push_back(w, &current_room);
+
 		while (++i < ft_size(e->rooms))
 		{
-			if (i)
-				;
-			// << ===
+			;
 		}
+		// push possibility
+
+		ft_delete_container(&begins);
 	}
-	ft_delete_container(&begins);
 }
 
 void			getways(t_lemin *e)
